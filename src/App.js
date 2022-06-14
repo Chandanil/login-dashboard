b1bcf20407c1eec99ceed8b12a0497a30c9b9b55
Chandanil <chandanee48@gmail.com>
2022-06-14 10:26:09 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Sidebar } from "./Components/Sidebar/sidebar";
+
+const App = ()=> {
+return 
+(
+    <div>
+   <div className="container">
+   <div className="row">
+        <div className="col-md-3">
+          <Sidebar/>
+        </div>
+    </div>
+   </div>
+</div>
+)
+}
+export default App;
---
+++ src/Components/Sidebar/sidebar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Sidebar = ()=> {
+    return 
+    (
+    <div classNme="cl-sidebar">
+logo here ggg
+    </div>
+    )
+}
